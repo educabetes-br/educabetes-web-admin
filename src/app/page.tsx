05916cd3727d9b46c5ebdc6b9e38133d405b71b4
Ramontaffa <taffarelramon1@gmail.com>
2025-03-20 11:08:36 -0300

    feat(model-list): created dialog for add new reports model. Also import assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import NewModelCard from "components/Newmodel";
 
 export default function Home() {
   //const session = useSession();
@@ -8,9 +9,9 @@ export default function Home() {
   // }
 
   return (
-    <div className="flex flex-1 flex-col h-full justify-around items-center bg-[#404AA0]">
+    <div className="flex flex-1 flex-col h-full justify-around items-center bg-slate-600">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-4xl font-bold">EducaBetes</h1>
+        <NewModelCard />
         
       </div>
     </div>
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,16 @@
 import Logo from './Logo.png';
+import eye from './ModelList/eye.svg';
+import download from './ModelList/download.svg';
+import unchecked from './ModelList/unchecked.svg';
+import checked from './ModelList/checked.svg';
+import plusIcon from './ModelList/plusIcon.svg';
 
-export { Logo };
+
+export { 
+  Logo,
+  eye,
+  download,
+  unchecked,
+  checked,
+  plusIcon,
+ };
